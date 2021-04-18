5216141f01562e4b35769c615f48504ddfddcd09
NimeshMittal <>
2021-04-18 20:23:46 +0000

    adding label entity

---
+++ entity/label.go
@@ -0,0 +1,11 @@
+package entity
+
+type Label struct {
+  Namespace string
+  Name string
+  Active bool
+  CreatedBy string
+  CreatedAt int64
+  UpdatedBy string
+  UpdatedAt int64
+}
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
   "fmt"
+  "n-labels/entity"
 )
 
 func main(){
   fmt.Println("hello labels!!")
+  e := entity.Label{}
+  fmt.Println(e)
 }
